2768e15ba5a8fabd6ad2205c47309e8a9201e47c
mjmjmjmjmjmjmj <936485466@qq.com>
2015-04-21 21:10:09 +0800

    search engine

---
+++ SearchEngine/searchpage.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<title>Simple PHP Search Engine - phphunger.com</title>
+</head>
+<body>
+<form action="search.php" method="get" >
+Please search here:
+    <input type="text" name="query" id="text"  />
+ <input type="submit" name="submit" id="search" value="Search" />
+</form>
+</body>
+</html>
